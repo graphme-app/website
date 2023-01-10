72af9e8692ee4f10e6fdc6d5b499f7bc40536883
Vincent <vincent.primault@gmail.com>
2023-01-10 16:34:05 +0100

    Add a sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import remarkDirective from "remark-directive"
-import remarkCalloutDirectives from "@microflash/remark-callout-directives"
+import remarkDirective from "remark-directive";
+import remarkCalloutDirectives from "@microflash/remark-callout-directives";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
+  site: 'https://graphme.app',
   markdown: {
     remarkPlugins: [remarkDirective, remarkCalloutDirectives],
-    extendDefaultPlugins: true,
+    extendDefaultPlugins: true
   },
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false
-      }
-    }),
-    react()
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), react(), sitemap()]
 });
